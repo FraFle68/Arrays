fa302920a9420f4a2c0ad340a72ff38e86921130
FraFle68 <frank.fleischer@wolfsburg.de>
2023-08-23 11:38:26 +0200

    finalised

--- src/main/java/ArrayIntrodution.java
+++ src/main/java/ArrayIntrodution.java
@@ -1,5 +1,59 @@
 public class ArrayIntrodution {
     public static void main(String[] args) {
 
+        // Task 1
+        int[] numbers = new int[10];
+        for(int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+        System.out.println("Task 1");
+        printArray(numbers);
+
+        // Task 2
+        System.out.println("Task 2");
+        System.out.println(ArraySum(numbers) + "\n");
+
+        // Task 3
+        System.out.println("Task 3");
+        System.out.println(highestNumber(numbers) + "\n");
+
+        // Task 4
+        int[] numbersHigh = new int[10];
+        for(int i = 0; i < numbersHigh.length; i++) {
+            numbersHigh[i] = i + 11;
+        }
+
+        // Task 5
+        System.out.println("Task 5");
+        int[] numbersAddition = new int [10];
+        for(int i = 0; i < numbers.length; i++) {
+            numbersAddition[i] = numbers[i] + numbersHigh[i];
+        }
+        printArray((numbersAddition));
+
+    }
+
+    public static int ArraySum(int[] numbers) {
+        int sum = 0;
+        for(int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+    public static int highestNumber(int[] numbers) {
+        int maximum = Integer.MIN_VALUE;
+        for(int number : numbers) {
+            if(number > maximum) {
+                maximum = number;
+            }
+        }
+        return maximum;
+    }
+
+    public static void printArray(int[] numbers) {
+        for(int number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println();
     }
 }
